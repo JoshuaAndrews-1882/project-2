38f93512294a1abbca56f28db0fc9f48a6bd81fd
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-31 14:33:58 +0000

    Add event listener for when game buttons are pressed and passed to the player id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-const userChoice = document.getElementById('player');
+const playerChoice = document.getElementById('player');
 const computerChoice = document.getElementById('computer');
 const result = document.getElementById('result');
 const playerWin = document.getElementById('won');
 const computerWin = document.getElementById('lost');
-const gameChoices = document.querySelectorAll('buttons');
+const gameChoices = document.querySelectorAll('button');
+
+let pChoice;
+let cChoice;
+let won = 0;
+let lost = 0;
+
+gameChoices.forEach(gameChoice => gameChoice.addEventListener('click', (e) => {
+    pChoice = e.target.id
+    playerChoice.innerHTML = pChoice
+}))
