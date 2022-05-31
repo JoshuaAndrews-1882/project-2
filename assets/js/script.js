f5888f73f5a3742394898de5ec2d559198aee7bd
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-31 14:11:23 +0000

    Clear JS

--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-const userChoice = document.getElementById('player')
-const computerChoice = document.getElementById('computer')
-const gameResult = document.getElementById('result')
-const gameChoices = document.querySelectorAll('button');
