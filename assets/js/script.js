6ff5d47e95aeb2395f51fe4f537082821a26e97b
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-31 14:24:30 +0000

    Add const and let values for JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const userChoice = document.getElementById('player');
+const computerChoice = document.getElementById('computer');
+const result = document.getElementById('result');
+const playerWin = document.getElementById('won');
+const computerWin = document.getElementById('lost');
+const gameChoices = document.querySelectorAll('buttons');
