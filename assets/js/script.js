8a4b912e0e9921d8da5eb8a4b7023dc77826612d
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-31 09:05:24 +0000

    Create const's to get the ids and buttons I need from indext.html

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const userChoice = document.getElementById('player')
+const computerChoice = document.getElementById('computer')
+const gameResult = document.getElementById('result')
+const gameChoices = document.querySelectorAll('button');
