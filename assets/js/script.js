4e8989ef5234f8decf64802f0c6239358b738017
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-06-02 14:44:15 +0000

    Use beautifytools.com to tidy up JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,149 +8,152 @@ let won = 0;
 let lose = 0;
 
 gameChoices.forEach(gameChoice => gameChoice.addEventListener('click', (e) => {
-    pChoice = e.target.id;
-    getComputerChoice();
-    compareChoices();
-    gameScore();
-    endGame();
+	pChoice = e.target.id;
+	getComputerChoice();
+	compareChoices();
+	gameScore();
+	endGame();
 }));
 
-function getComputerChoice () {
-    const randomChoice = Math.floor(Math.random() * 5);
+function getComputerChoice() {
+	const randomChoice = Math.floor(Math.random() * 5);
 
-    switch(randomChoice) {
-        case 0:
-            cChoice = 'rock';
-            break;
-        case 1:
-            cChoice = 'paper';
-            break;
-        case 2:
-            cChoice = 'scissors';
-            break;
-        case 3:
-            cChoice = 'lizard';
-            break;
-        case 4:
-            cChoice = 'spock';
-    }
+	switch (randomChoice) {
+		case 0:
+			cChoice = 'rock';
+			break;
+		case 1:
+			cChoice = 'paper';
+			break;
+		case 2:
+			cChoice = 'scissors';
+			break;
+		case 3:
+			cChoice = 'lizard';
+			break;
+		case 4:
+			cChoice = 'spock';
+	}
 }
 
 function compareChoices() {
-    if (pChoice === cChoice) {
-        winner = "Stalemate, you picked the same";   
-}
-    if (pChoice === 'rock'  && cChoice === 'paper') {
-        winner = "Rock is covered by Paper, you lose!";
-        lose++;
-    }
-    if (pChoice === 'rock' && cChoice === 'scissors') {
-        winner = "Rock crushes Scissors, you win!";
-        won++;
-    }
-    if (pChoice === 'rock' && cChoice === 'lizard') {
-        winner = "Rock crushes the Lizard, you win!";
-        won++;
-    }
-    if (pChoice === 'rock' && cChoice === 'spock') {
-        winner = "Rock is vaporized by Spock, you lose!";
-        lose++;
-    }
-    if (pChoice === 'paper' && cChoice === 'rock') {
-        winner = "Paper covers Rock, you win!";
-        won++;
-    }
-    if (pChoice === 'paper' && cChoice === 'scissors') {
-        winner = "Paper is cut by Scissors, you lose!";
-        lose++;
-    }
-    if (pChoice === 'paper' && cChoice === 'lizard') {
-        winner = "Paper is eaten by the Lizard, you lose!";
-        lose++;
-    }
-    if (pChoice === 'paper' && cChoice === 'spock') {
-        winner = "Paper disproves Spock, you win!";
-        won++;
-    }
-    if (pChoice === 'scissors' && cChoice === 'rock') {
-        winner = "Scissors gets smashed by Rock, you lose!";
-        lose++;
-    }
-    if (pChoice === 'scissors' && cChoice === 'paper') {
-        winner = "Scissors cuts Paper, you win!";
-        won++;
-    }
-    if (pChoice === 'scissors' && cChoice === 'lizard') {
-        winner = "Scissors decapitates lizard, you win!";
-        won++;
-    }
-    if (pChoice === 'scissors' && cChoice === 'spock') {
-        winner = "Scissors are smashed by Spock, you lose!";
-        lose++;
-    }
-    if (pChoice === 'lizard' && cChoice === 'rock') {
-        winner = "Your lizard is crushed by Rock, you lose!";
-        lose++;
-    }
-    if (pChoice === 'lizard' && cChoice === 'paper') {
-        winner = "Your lizard eats the paper, you win!";
-        won++;
-    }
-    if (pChoice === 'lizard' && cChoice === 'scissors') {
-        winner = "Your lizard is decapitated by Scissors, you lose!";
-        lose++;
-    }
-    if (pChoice === 'lizard' && cChoice === 'spock') {
-        winner = "Your lizard poisons Spock, you win!";
-        won++;
-    }
-    if (pChoice === 'spock' && cChoice === 'rock') {
-        winner = "Spock vaporizes rock, you win!";
-        won++;
-    }
-    if (pChoice === 'spock' && cChoice === 'paper') {
-        winner = "Spock is disproved by Paper, you lose!";
-        lose++;
-    }
-    if (pChoice === 'spock' && cChoice === 'scissors') {
-        winner = "Spock smashed scissors, you win!";
-        won++;
-    }
-    if (pChoice === 'spock' && cChoice === 'lizard') {
-        winner = "Spock is poisoned by the Lizard, you lose!";
-        lose++;
-    }
-    gameResult.innerHTML = winner;
+	if (pChoice === cChoice) {
+		winner = "Stalemate, you picked the same";
+	}
+	if (pChoice === 'rock' && cChoice === 'paper') {
+		winner = "Rock is covered by Paper, you lose!";
+		lose++;
+	}
+	if (pChoice === 'rock' && cChoice === 'scissors') {
+		winner = "Rock crushes Scissors, you win!";
+		won++;
+	}
+	if (pChoice === 'rock' && cChoice === 'lizard') {
+		winner = "Rock crushes the Lizard, you win!";
+		won++;
+	}
+	if (pChoice === 'rock' && cChoice === 'spock') {
+		winner = "Rock is vaporized by Spock, you lose!";
+		lose++;
+	}
+	if (pChoice === 'paper' && cChoice === 'rock') {
+		winner = "Paper covers Rock, you win!";
+		won++;
+	}
+	if (pChoice === 'paper' && cChoice === 'scissors') {
+		winner = "Paper is cut by Scissors, you lose!";
+		lose++;
+	}
+	if (pChoice === 'paper' && cChoice === 'lizard') {
+		winner = "Paper is eaten by the Lizard, you lose!";
+		lose++;
+	}
+	if (pChoice === 'paper' && cChoice === 'spock') {
+		winner = "Paper disproves Spock, you win!";
+		won++;
+	}
+	if (pChoice === 'scissors' && cChoice === 'rock') {
+		winner = "Scissors gets smashed by Rock, you lose!";
+		lose++;
+	}
+	if (pChoice === 'scissors' && cChoice === 'paper') {
+		winner = "Scissors cuts Paper, you win!";
+		won++;
+	}
+	if (pChoice === 'scissors' && cChoice === 'lizard') {
+		winner = "Scissors decapitates lizard, you win!";
+		won++;
+	}
+	if (pChoice === 'scissors' && cChoice === 'spock') {
+		winner = "Scissors are smashed by Spock, you lose!";
+		lose++;
+	}
+	if (pChoice === 'lizard' && cChoice === 'rock') {
+		winner = "Your lizard is crushed by Rock, you lose!";
+		lose++;
+	}
+	if (pChoice === 'lizard' && cChoice === 'paper') {
+		winner = "Your lizard eats the paper, you win!";
+		won++;
+	}
+	if (pChoice === 'lizard' && cChoice === 'scissors') {
+		winner = "Your lizard is decapitated by Scissors, you lose!";
+		lose++;
+	}
+	if (pChoice === 'lizard' && cChoice === 'spock') {
+		winner = "Your lizard poisons Spock, you win!";
+		won++;
+	}
+	if (pChoice === 'spock' && cChoice === 'rock') {
+		winner = "Spock vaporizes rock, you win!";
+		won++;
+	}
+	if (pChoice === 'spock' && cChoice === 'paper') {
+		winner = "Spock is disproved by Paper, you lose!";
+		lose++;
+	}
+	if (pChoice === 'spock' && cChoice === 'scissors') {
+		winner = "Spock smashed scissors, you win!";
+		won++;
+	}
+	if (pChoice === 'spock' && cChoice === 'lizard') {
+		winner = "Spock is poisoned by the Lizard, you lose!";
+		lose++;
+	}
+	gameResult.innerHTML = winner;
 }
 
 function gameScore() {
-    document.getElementById('won').innerHTML = won;
-    document.getElementById('lose').innerHTML = lose;
-}
-    
-function endGame() {
-    if (won === 10) {
-            document.getElementById('result').innerHTML = 'You win the game';
-            document.getElementById('won').innerHTML = (won = 0);
-            document.getElementById('lose').innerHTML = (lose = 0);
-    }
-    if (lose === 10) {
-            document.getElementById('result').innerHTML = 'You lost the game';
-            document.getElementById('won').innerHTML = (won = 0);
-            document.getElementById('lose').innerHTML = (lose = 0);
-    }  
+	document.getElementById('won').innerHTML = won;
+	document.getElementById('lose').innerHTML = lose;
 }
 
+function endGame() {
+	if (won === 10) {
+		document.getElementById('result').innerHTML = 'You win the game';
+		document.getElementById('won').innerHTML = (won = 0);
+		document.getElementById('lose').innerHTML = (lose = 0);
+	}
+	if (lose === 10) {
+		document.getElementById('result').innerHTML = 'You lost the game';
+		document.getElementById('won').innerHTML = (won = 0);
+		document.getElementById('lose').innerHTML = (lose = 0);
+	}
+}
+
+
+// Taken from https://www.w3schools.com/howto/howto_css_modal_images.asp
+// Opens up rules box using an image
 var modal = document.getElementById("rules-modal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the image and insert it inside the modal
 var img = document.getElementById("rules-game");
 var modalImg = document.getElementById("image");
 var captionText = document.getElementById("rules-header");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+img.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
 };
 
 // Get the <span> element that closes the modal
@@ -158,5 +161,5 @@ var span = document.getElementsByClassName("rules-close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+	modal.style.display = "none";
 };
